a13b95e3afe6447354015e146da56f419ccdc705
efecun-dev <mecgithub@gmail.com>
2026-07-08 19:08:42 +0300

    Initial commit

---
+++ lib/db.ts
@@ -0,0 +1,6 @@
+// lib/db.ts
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export const db = new PrismaClient({ adapter });
