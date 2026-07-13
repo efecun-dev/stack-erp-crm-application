9dbe1c2aff475bfae3e6e3baf6d8758782328817
efecun-dev <mecgithub@gmail.com>
2026-07-13 14:06:53 +0300

    feat(dash-info-box): design has been revised

--- components/dashboard/DashInfoBox.tsx
+++ components/dashboard/DashInfoBox.tsx
@@ -16,10 +16,10 @@ export default function DashInfoBox({
     <div className="rounded-xl border border-[#D7ECEF] bg-white p-5">
       <p className="text-sm text-gray-500">{title}</p>
 
-      <h4 className="mt-1 text-2xl font-semibold text-[#0B2E33]">{content}</h4>
+      <h4 className="mt-1 text-3xl font-semibold text-[#0B2E33]">{content}</h4>
 
       <p
-        className={`mt-2 text-sm font-light ${
+        className={`text-sm font-light ${
           subtitle.type === "success"
             ? "text-green-700"
             : subtitle.type === "danger"
